fe9aa66a61295ed5010e40f0e89c81874b243c7b
Lucas Samel <lucas.samel97@gmail.com>
2020-12-06 21:20:36 -0300

    Tela de CadastrarFilmeFragment.kt

--- app/src/main/java/lucassamel/br/at_fundamentosamdroid/model/Filme.kt
+++ app/src/main/java/lucassamel/br/at_fundamentosamdroid/model/Filme.kt
@@ -1,10 +1,17 @@
 package lucassamel.br.at_fundamentosamdroid.model
 
+import android.util.Log
+import kotlin.math.log
+
 class Filme (
     var nomeFilme : String,
-    var anoLancamento : Int
+    var anoLancamento : String
         ){
     override fun toString(): String {
         return "$nomeFilme , $anoLancamento"
+    }
+
+    fun store(){
+        Log.i("Filme","$nomeFilme registrado com sucesso")
     }
 }
--- app/src/main/java/lucassamel/br/at_fundamentosamdroid/ui/CadastrarFilmeFragment.kt
+++ app/src/main/java/lucassamel/br/at_fundamentosamdroid/ui/CadastrarFilmeFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_cadastrar_filme.*
 import lucassamel.br.at_fundamentosamdroid.R
+import lucassamel.br.at_fundamentosamdroid.model.Filme
 
 class CadastrarFilmeFragment : Fragment() {
 
@@ -18,5 +21,23 @@ class CadastrarFilmeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_cadastrar_filme, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnCadastrarFilme.setOnClickListener {
+            var nomeFilme = editTextNomeFilme.text.toString()
+            var anoLancamento = editTextAnoFilme.text.toString()
+
+            var filme = Filme(nomeFilme, anoLancamento)
+            filme.store()
+            Toast.makeText(
+                requireContext(),
+                "Filme Cadastrado com Sucesso!",
+                Toast.LENGTH_LONG
+            ).show()
+
+        }
+
+    }
 
 }
